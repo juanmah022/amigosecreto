e11b6249c62220829759719ad72cea016483584b
juan <kincaban9@gmail.com>
2025-03-16 14:52:47 -0500

    final

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-console.log ();
 
-function agregarNombre() {
-    let nombreAmigo = document.querySelector("amigo").Value;
-    if (nombreAmigo === "") {
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo.trim() === "") {
         alert("por favor, inserte nombre");     
     } else { 
         amigos.push(nombreAmigo);
-        document.querySelector ("amigo").Value = "";
+        document.querySelector("#amigo").value = "";
+        mostrarLista();
     }
 }
 
 function mostrarLista () {
-    let lista = document.querySelector("listaAmigos");
+    let lista = document.querySelector("#listaAmigos");
     lista.innerHTML = "";
     
     for (let i = 0; i < amigos.length; i++) {
         const elemento = amigos [i]; 
-        let listaHtml = document.createElement ("li");
+        let listaHtml = document.createElement("li");
         listaHtml.textContent = elemento;
+        
         lista.appendChild(listaHtml);
     }
 
+
+
+}
+function sortearAmigo () {
+    if  (amigos.length === 0) {
+        alert ("Ingrese un nombre");
+    }
+     else { 
+        let selecionAleatoria = Math.floor(Math.random() * (amigos.length));
+        let resultado = document.querySelector("#resultado");
+        resultado.innerHTML=amigos[selecionAleatoria];
+    }  
 }
